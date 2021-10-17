76958d4e704568c03b44cf453fd2e4a303f26639
rokDaniel <92662251+rokDaniel@users.noreply.github.com>
2021-10-17 09:16:34 +0300

    Initial commit

---
+++ src/spring/project/dao/CustomerDAO.java
@@ -0,0 +1,16 @@
+package spring.project.dao;
+
+import java.util.List;
+
+import spring.project.entity.Customer;
+
+public interface CustomerDAO {
+	
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+}
---
+++ src/spring/project/service/CustomerService.java
@@ -0,0 +1,16 @@
+package spring.project.service;
+
+import java.util.List;
+
+import spring.project.entity.Customer;
+
+public interface CustomerService {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+}
